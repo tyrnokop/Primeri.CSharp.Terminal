b164df582bf858faacc088acd1d20a45cd33f786
tyrnokop <tyrnokop@abv.bg>
2016-10-09 23:55:46 +0300

    Модул 02 Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			//Дефиниране на променливи
+
+			//Събиране на числа
+
+			//Писане в конзолата
+		}
+	}
+}
